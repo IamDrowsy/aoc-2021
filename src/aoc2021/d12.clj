af7faf0b57d025e7c1cd412c5f88375faf5b779e
Albrecht Schmidt <mail@albrechtschmidt.de>
2021-12-12 12:38:45 +0100

    added some optimization and reducers

--- src/aoc2021/d12.clj
+++ src/aoc2021/d12.clj
@@ -2,7 +2,9 @@
   (:require [aoc2021.util :refer [get-input check fix-point]]
             [com.rpl.specter :refer :all]
             [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.core.reducers :as r]
+            [criterium.core :refer [quick-bench]]))
 
 (defn add-entry [cave-map start end]
   (if (or (= end "start")
@@ -29,30 +31,43 @@
   (= "end" (peek path)))
 
 (defn step-path [cave-map allow-twice? path]
-  (if (finished? path)
-    #{path}
-    (let [current (peek path)
-          {:keys [visited-small visited-twice]} (meta path)
-          new-visited-twice (or visited-twice (visited-small current))
-          possible-next (set/difference (cave-map current #{})
-                                        (if (or (not allow-twice?) new-visited-twice) visited-small #{}))
-          add-step (if (small? current)
-                     (let [new-meta {:visited-small (conj visited-small current)
-                                     :visited-twice new-visited-twice}]
-                       (fn [next] (with-meta (conj path next) new-meta)))
-                     (partial conj path))]
-      (set (map add-step possible-next)))))
+  (let [current (peek path)
+        {:keys [visited-small visited-twice]} (meta path)
+        new-visited-twice (or visited-twice (visited-small current))
+        possible-next (set/difference (cave-map current #{})
+                                      (if (or (not allow-twice?) new-visited-twice) visited-small #{}))
+        add-step (if (small? current)
+                   (let [new-meta {:visited-small (conj visited-small current)
+                                   :visited-twice new-visited-twice}]
+                     (fn [next] (with-meta (conj path next) new-meta)))
+                   (partial conj path))]
+    (r/map add-step possible-next)))
 
 (defn step-paths [cave-map allow-twice? path-set]
-  (reduce set/union (map (partial step-path cave-map allow-twice?) path-set)))
+  (r/foldcat
+    (r/mapcat (partial step-path cave-map allow-twice?) path-set)))
 
 (def start-path
   (with-meta ["start"] {:visited-small #{}
                         :visited-twice false}))
 
+(defn complete-path-count-fn [cave-map allow-twice?]
+  (let [path# (volatile! 0)
+        f (partial step-paths cave-map allow-twice?)]
+    (fn [open-path]
+      (if (number? open-path)
+        open-path
+        (let [new-paths (f open-path)
+              unfinished (vec (remove finished? new-paths))
+              finished# (count (filter finished? new-paths))]
+          (vswap! path# + finished#)
+          (if (empty? unfinished)
+            @path#
+            unfinished))))))
+
 (defn solve [input allow-twice?]
-  (count (fix-point (partial step-paths (parse-cave-map input) allow-twice?)
-                    #{start-path})))
+  (fix-point (complete-path-count-fn (parse-cave-map input) allow-twice?)
+             [start-path]))
 
 (defn solve-1 [input]
   (solve input false))
